dc0e168837d2185043a84771a3e63c6b5ac5e81d
Riccardo Corradin <riccardo.corradin@live.com>
2019-11-07 13:43:35 +0100

    Added DB Context class

---
+++ AcceleratorMetrics/Models/MetricsContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AcceleratorMetrics.Models
+{
+    public class MetricsContext : DbContext
+    {
+        public MetricsContext(DbContextOptions<MetricsContext> options) : base(options)
+        { }
+
+        public DbSet<Deployment> Deployments { get; set; }
+    }
+}
