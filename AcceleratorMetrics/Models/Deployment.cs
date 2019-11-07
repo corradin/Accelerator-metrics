a336ae6d8f21b81a8c3c8fc95568f6d34c8fe0c8
Riccardo Corradin <riccardo.corradin@live.com>
2019-11-07 12:47:21 +0100

    Created deployment models and controller

---
+++ AcceleratorMetrics/Models/Deployment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AcceleratorMetrics.Models
+{
+    public class Deployment
+    {
+        public int ID { get; set; }
+        public DateTime Moment { get; set; }
+        public string VersionNumber { get; set; }
+        public bool RolledBack { get; set; }
+        public bool Released { get; set; }
+    }
+}
